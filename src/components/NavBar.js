7fefbdc2b04dc30ea5dd422bcd916bd58749b295
Alejandro Salas <asalas.wd@outlook.com>
2020-03-06 22:23:36 -0500

    create header component

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <header>
+      <ul>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  );
+}
+
+export default NavBar;
