2673a36910fc28d436e5fe5f9b78ef3aef4ecd02
Alejandro Salas <asalas.wd@outlook.com>
2020-03-29 12:28:35 -0400

    create single to-do item component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Goals from './Goals';
+import ToDoItem from './ToDoItem';
 
 function MainContent() {  
 
   return(
     <div className='goals'>
       <h2 >Goals for 2020:</h2>
-      <Goals />
+      <ul>
+        <ToDoItem />
+      </ul>
     </div>
   );
 }
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ToDoItem() {  
+
+  return(    
+    <li><input class='cbox' type='checkbox' />Become a Web Developer</li>
+  )
+}
+
+export default ToDoItem
